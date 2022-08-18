97ec6464100a6a44d6cc844bb89465571cd891cf
Tomek Nowicki <t.nowicki@pollub.pl>
2022-08-18 14:30:03 +0200

    Lab13

---
+++ src/main/java/pwo/lab13/Text.java
@@ -0,0 +1,15 @@
+package pwo.lab13;
+
+public class Text {
+   
+    static boolean cont5Dig(String str){
+        return str.replaceAll("[^0-9]", "").length()==5;
+    } 
+}
+
+
+
+
+    //Nie zawiera cyfr
+    
+    //Zawiera tylko cyfry
